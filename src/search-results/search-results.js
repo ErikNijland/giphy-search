eef4b29d43eb0ccc3c9ce43e05de67dee17fab71
Erik Nijland <e.n.nijland@gmail.com>
2019-11-22 00:07:57 +0100

    add placeholder code to start searching

--- src/search-results/search-results.js
+++ src/search-results/search-results.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 export default class SearchResults extends React.Component {
+  componentDidMount() {
+    this.search(this.props.query);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.query !== prevProps.query) {
+      this.search(this.props.query);
+    }
+  }
+
+  search(query) {
+    console.log('go searching', query);
+  }
+
   render() {
     return (
       <div>search results here please: {this.props.query}</div>
