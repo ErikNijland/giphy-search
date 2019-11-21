03b6506545b99383f6e7cf6804f496aafc30ee65
Erik Nijland <e.n.nijland@gmail.com>
2019-11-21 20:52:08 +0100

    clean up of default crap plus my first component

---
+++ src/search-form/search-form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function SearchForm() {
+  return (
+    <form>
+      <label>
+        Search query
+        <input />
+      </label>
+    </form>
+  );
+}
