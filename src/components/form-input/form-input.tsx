4ef60f3db71dd3f154b1e1e70c261320fc2e3b01
Erik Nijland <e.n.nijland@gmail.com>
2019-11-30 17:12:47 +0100

    form-input component: add optional add with fallback and some styling

--- src/components/form-input/form-input.tsx
+++ src/components/form-input/form-input.tsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, {useState} from 'react';
 import {IFormInputValue} from "../../types/form-input-value";
+import './form-input.css';
 
 interface IProps {
   autofocus: boolean;
   label: string;
   formInputValue: IFormInputValue;
+  id?: string;
 }
 
 export function FormInput(props: IProps) {
+  const [ randomId ] = useState('randomId-' + Math.random());
+  const htmlId = props.id || randomId;
+
   return (
-    <label>
-      {props.label}
-      <input {...props.formInputValue} autoFocus={props.autofocus} />
-    </label>
+    <div className="FormInput">
+      <label className="FormInput__Label" htmlFor={htmlId}>{props.label}</label>
+      <input id={htmlId} className="FormInput__Input" {...props.formInputValue} autoFocus={props.autofocus} />
+    </div>
   );
 }
 
