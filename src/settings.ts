379e08e527670726104b04950a0c71ed6566426a
Erik Nijland <e.n.nijland@gmail.com>
2019-11-30 22:06:26 +0100

    change the default setting to kid friendly

--- src/settings.ts
+++ src/settings.ts
@@ -3,5 +3,5 @@ import {ISettings} from './types/settings';
 export const settings: ISettings = {
   giphyApiKey: "NvklHHolU7yM5ES8QV9TYPJzwtNQBld3",
   imagesPerPage: 20,
-  rating: 'r',
+  rating: 'g',
 };
