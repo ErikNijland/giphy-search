8508cb0c7bf7db7f2fdb4b919f3b44660da8547f
Erik Nijland <e.n.nijland@gmail.com>
2019-11-30 18:42:23 +0100

    make the link in the header conditional

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, matchPath, useLocation} from "react-router-dom";
 import "./header.css";
 
 export default function Header() {
+  const location = useLocation();
+  const showLink = matchPath(location.pathname, { path: "/", exact: true }) === null;
+  const header = "GIPHY image search";
+
   return (
     <header className="Header">
-      <h1 className="Header__Title"><Link to="/" className="Header__Link">GIPHY image search</Link></h1>
+      <h1 className="Header__Title">
+        {showLink
+          ? <Link to="/" className="Header__Link">{header}</Link>
+          : header
+        }
+      </h1>
 
       <nav className="Header_Menu">
         <Link to="/settings" className="Header__Link">Settings</Link>
